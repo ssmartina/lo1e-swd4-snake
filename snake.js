4c3e4425010e103e3908513242e223d126d59045
ssmartina <94383398+ssmartina@users.noreply.github.com>
2022-09-06 14:26:08 +0200

    snake beginning

---
+++ snake.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("snake");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, 400, 400);
+
+ctx.fillStyle = "green";
+ctx.fillRect(200, 200, 20, 20);
