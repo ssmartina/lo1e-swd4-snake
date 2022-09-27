49087133f8d1001091de726b524e3748858fcc0b
ssmartina <94383398+ssmartina@users.noreply.github.com>
2022-09-27 17:31:20 +0200

    Snake can eat apples

--- snake.js
+++ snake.js
@@ -1,8 +1,16 @@
 let canvas = document.getElementById("snake");
 let ctx = canvas.getContext("2d");
 
-let snakeX = 200;
-let snakeY = 200;
+let snake = [
+    {
+        x: 200,
+        y: 200
+    },
+    {
+        x: 220,
+        y: 200
+    }
+];
 
 let foodX;
 let foodY;
@@ -17,36 +25,60 @@ function drawBackground() {
 
 function drawSnake() {
     ctx.fillStyle = "green";
-    ctx.fillRect(snakeX, snakeY, 20, 20);
+
+    for (let index = 0; index < snake.length; index++) {
+        ctx.fillRect(snake[index].x, snake[index].y, 20, 20);
+    }
+
 }
 
 function update() {
+
+    let tailIndex = snake.length - 1;
+    let tailX = snake[tailIndex].x;
+    let tailY = snake[tailIndex].y;
+
+    if (direction != null) {
+        for(let index = snake.length - 1; index > 0; index--) {
+            snake[index].x = snake[index - 1].x;
+            snake[index].y = snake[index - 1].y;
+        }
+    }
+
     if(direction == 'right') {
-        if (snakeX < 380) {
-            snakeX += 20;
+        if (snake[0].x < 380) {
+            snake[0].x += 20;
         } else {
             gameOver();
         }
     } else if(direction == 'left') {
-        if(snakeX > 0) {
-            snakeX -= 20;
+        if (snake[0].x > 0) {
+            snake[0].x -= 20;
         } else {
             gameOver();
         }
     } else if(direction == 'up') {
-        if (snakeY > 0) {
-            snakeY -= 20;
+        if (snake[0].y > 0) {
+            snake[0].y -= 20;
         } else {
             gameOver();
         }
     } else if(direction == 'down') {
-        if (snakeY < 380) {
-            snakeY += 20;
+        if (snake[0].y < 380) {
+            snake[0].y += 20;
         } else {
             gameOver();
         }
     }
 
+    if (foodX == snake[0].x && snake[0].y == foodY) {
+        snake.push ({
+            x: tailX,
+            y: tailY
+        });
+        
+        spawnFood();
+    }
 
     drawBackground();
     drawSnake();
